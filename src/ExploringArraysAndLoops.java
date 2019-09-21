0de5010eb781c4ca5edda53f2ee4d60fdd06bb24
Esther <esther191299@gmail.com>
2019-09-21 19:58:36 +0200

    First Commit

---
+++ src/ExploringArraysAndLoops.java
@@ -0,0 +1,38 @@
+
+public class ExploringArraysAndLoops {
+
+	public static void main(String[] args) {
+		String[] storedLetter = new String [11];
+		storedLetter [0] = " ";
+		storedLetter [1] = "A";
+		storedLetter [2] = "B";
+		storedLetter [3] = "C";
+		storedLetter [4] = "D";
+		storedLetter [5] = "E";
+		storedLetter [6] = "F";
+		storedLetter [7] = "G";
+		storedLetter [8] = "H";
+		storedLetter [9] = "I";
+		storedLetter [10] ="J";
+		for (int i = 2; i <= 8; i++)
+			System.out.print(storedLetter[i]);
+
+		
+		char[] storedCharacter = new char [11];
+		storedCharacter [0] = ' ';
+		storedCharacter [1] = 'S';
+		storedCharacter [2] = '.';
+		storedCharacter [3] = '.';
+		storedCharacter [4] = '.';
+		storedCharacter [5] = '.';
+		storedCharacter [6] = 'E';
+		storedCharacter [7] = '.';
+		storedCharacter [8] = '.';
+		storedCharacter [9] = '.';
+		storedCharacter [10] ='T';
+		for (int i = 0; i <= 6; i++)
+			System.out.println(storedCharacter[i]);
+
+
+	}
+}
